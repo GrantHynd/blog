f6933d3c99a5f1e31a8dc8ad2f37047222fd06ec
Grant Hynd <grant.hynd@bt.com>
2021-09-06 00:47:26 +0100

    Add terraform setup config

---
+++ infrastructure/terraform/k8s-cluster/terraform.tf
@@ -0,0 +1,80 @@
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "1.20.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.3.0"
+    }
+  }
+}
+
+provider "linode" {
+  token = var.linode_token
+}
+
+# https://registry.terraform.io/providers/linode/linode/latest/docs/resources/lke_cluster
+resource "linode_lke_cluster" "prod_cluster" {
+  label       = var.cluster_name
+  k8s_version = var.k8s_version
+  region      = var.region
+  tags        = ["PROD"]
+
+  pool {
+      type  = var.server_type_node
+      count = var.nodes
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  depends_on   = [linode_lke_cluster.prod_cluster]
+  filename     = "kubeconfig"
+  content      = base64decode(linode_lke_cluster.prod_cluster.kubeconfig)
+}
+
+# Setup helm connection to cluster 
+provider "helm" {
+  kubernetes {
+    config_path = "./kubeconfig"
+  }
+}
+
+# TODO switch to use: https://charts.bitnami.com/bitnami
+# resource "helm_release" "ingress_nginx_release" {
+#   depends_on   = [local_file.kubeconfig]
+#   name             = "ingress-nginx"
+#   namespace        = "default"
+#   chart            = "ingress-nginx"
+#   version          = "4.0.1"
+#   repository       = "https://kubernetes.github.io/ingress-nginx"
+#   wait            = true
+#   cleanup_on_fail = true
+# }
+
+resource "helm_release" "blog_release" {
+  depends_on   = [local_file.kubeconfig]
+  name             = "blog"
+  namespace        = "default"
+  chart            = "blog"
+  version          = var.blog_chart_version
+  repository       = "https://granthynd.github.io/helm-charts"
+  wait            = true
+  cleanup_on_fail = true
+}
+
+# # Setup blog domain
+resource "linode_domain" "blog_domain" {
+  domain = "granthynd.com"
+  soa_email = "grant.hynd@gmail.com"
+  type = "master"
+}
+
+resource "linode_domain_record" "blog_domain_record" {
+  domain_id = linode_domain.blog_domain.id
+  name = "www"
+  record_type = "CNAME"
+  target = linode_domain.blog_domain.domain
+  ttl_sec = 300
+}
