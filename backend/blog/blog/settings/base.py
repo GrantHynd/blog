2652301dbb362363f4c05b3fbc4d4f929a29dbdb
Grant Hynd <grant.hynd@bt.com>
2021-12-28 23:19:48 +0000

    Fix domain default

--- backend/blog/blog/settings/base.py
+++ backend/blog/blog/settings/base.py
@@ -166,4 +166,4 @@ WAGTAIL_SITE_NAME = "blog"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = "https://" + env("BACKEND_DOMAIN", default=None)
+BASE_URL = "https://" + env("BACKEND_DOMAIN", default="")
